186072d72cbd553de96dd1f24918fff7d6d21d66
Flávio Brusamolin <flaviobrusamolin@gec.inatel.br>
2020-10-10 11:45:55 -0300

    feat: encircle add member controller with try catch

--- api/src/data/protocols/db/team/load-member-by-id-repository.ts
+++ api/src/data/protocols/db/team/load-member-by-id-repository.ts
@@ -1,5 +1,5 @@
 import { Member } from '../../../../domain/models/team'
 
 export interface LoadMemberByIdRepository {
-  loadMemberById: (teamId:string, memberId: string) => Promise<Member>
+  loadMemberById: (teamId: string, memberId: string) => Promise<Member>
 }
--- api/src/presentation/controllers/team/add-member-controller.ts
+++ api/src/presentation/controllers/team/add-member-controller.ts
@@ -3,7 +3,7 @@ import { LoadTeamById } from '../../../domain/use-cases/team/load-team-by-id'
 import { CheckMemberPermission } from '../../../domain/use-cases/team/check-member-permission'
 import { LoadUserByEmail } from '../../../domain/use-cases/user/load-user-by-email'
 import { AddMember } from '../../../domain/use-cases/team/add-member'
-import { badRequest, conflict, created, forbidden, notFound } from '../../helpers/http-helper'
+import { badRequest, conflict, created, forbidden, notFound, serverError } from '../../helpers/http-helper'
 import { Controller, HttpRequest, HttpResponse, Validator } from '../../protocols'
 import { DuplicateTeamMemberError, ResourceNotFoundError } from '../../errors'
 
@@ -22,38 +22,43 @@ export class AddMemberController implements Controller {
   ) {}
 
   public async handle (httpRequest: HttpRequest<AddMemberContract>): Promise<HttpResponse> {
-    const error = this.validator.validate(httpRequest.body)
-    if (error) {
-      return badRequest(error)
+    try {
+      const error = this.validator.validate(httpRequest.body)
+      if (error) {
+        return badRequest(error)
+      }
+
+      const { userId } = httpRequest
+      const { teamId } = httpRequest.params
+      const { email, role } = httpRequest.body
+
+      const team = await this.loadTeamById.load(teamId)
+      if (!team) {
+        return notFound(new ResourceNotFoundError('id'))
+      }
+
+      const hasPermission = await this.checkMemberPermission.check(teamId, userId, [Role.ADVANCED])
+      if (!hasPermission) {
+        return forbidden()
+      }
+
+      const user = await this.loadUserByEmail.load(email)
+      if (!user) {
+        return notFound(new ResourceNotFoundError('email'))
+      }
+
+      const updatedTeam = await this.addMember.add(teamId, {
+        userId: user.id,
+        role
+      })
+      if (!updatedTeam) {
+        return conflict(new DuplicateTeamMemberError(email))
+      }
+
+      return created(updatedTeam)
+    } catch (error) {
+      console.error(error)
+      return serverError()
     }
-
-    const { userId } = httpRequest
-    const { teamId } = httpRequest.params
-    const { email, role } = httpRequest.body
-
-    const team = await this.loadTeamById.load(teamId)
-    if (!team) {
-      return notFound(new ResourceNotFoundError('id'))
-    }
-
-    const hasPermission = await this.checkMemberPermission.check(teamId, userId, [Role.ADVANCED])
-    if (!hasPermission) {
-      return forbidden()
-    }
-
-    const user = await this.loadUserByEmail.load(email)
-    if (!user) {
-      return notFound(new ResourceNotFoundError('email'))
-    }
-
-    const updatedTeam = await this.addMember.add(teamId, {
-      userId: user.id,
-      role
-    })
-    if (!updatedTeam) {
-      return conflict(new DuplicateTeamMemberError(email))
-    }
-
-    return created(updatedTeam)
   }
 }
