c9d46bdf71ec1f7731b0df9d0841e14f70e5db97
Flavio Brito <flavio.brito@sensedia.com>
2020-10-17 17:25:35 +0000

    fix: change wrong interface parameter

--- api/src/domain/use-cases/collection/load-collections.ts
+++ api/src/domain/use-cases/collection/load-collections.ts
@@ -1,5 +1,5 @@
 import { Collection } from '../../models/collection'
 
 export interface LoadCollections {
-  load: (collectionId: string) => Promise<Collection[]>
+  load: (accessGroupIds: string[]) => Promise<Collection[]>
 }
