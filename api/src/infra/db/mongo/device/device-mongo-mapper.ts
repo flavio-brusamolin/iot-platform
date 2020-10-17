5cbf059721d5bf10a7be50aa4a0959ebdf1cca99
Flavio Brito <flavio.brito@sensedia.com>
2020-10-17 17:33:53 +0000

    fix: remove unexpected return field

--- api/src/infra/db/mongo/device/device-mongo-mapper.ts
+++ api/src/infra/db/mongo/device/device-mongo-mapper.ts
@@ -2,15 +2,15 @@ import { MongoDevice } from './device-mongo-model'
 import { Device } from '../../../../domain/models/device'
 
 export default {
-  toEntity: ({ _id, collectionId, accessGroupId, name, protocol, mqttInfo }: MongoDevice): Device => ({
+  toEntity: ({ _id, collectionId, accessGroupId, name, protocol, mqttInfo: { topic, brokerId } }: MongoDevice): Device => ({
     id: _id.toString(),
     collectionId: collectionId.toString(),
     accessGroupId: accessGroupId.toString(),
     name,
     protocol,
     mqttInfo: {
-      ...mqttInfo,
-      brokerId: mqttInfo.brokerId.toString()
+      topic,
+      brokerId: brokerId.toString()
     }
   })
 }
