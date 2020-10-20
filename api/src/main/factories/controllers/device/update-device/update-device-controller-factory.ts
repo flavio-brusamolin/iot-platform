04a9db6642c53af93dfd7868beb4d257c3e13b1a
Flavio Brito <flavio.brito@sensedia.com>
2020-10-20 00:10:08 +0000

    fix: add required validators and broker id validator

--- api/src/main/factories/controllers/device/update-device/update-device-controller-factory.ts
+++ api/src/main/factories/controllers/device/update-device/update-device-controller-factory.ts
@@ -2,6 +2,7 @@ import { makeDbCheckMemberPermission } from '../../../use-cases/team/db-check-me
 import { UpdateDeviceController } from '../../../../../presentation/controllers/device/update-device-controller'
 import { makeDbLoadDeviceById } from '../../../use-cases/device/db-load-device-by-id-factory'
 import { makeDbUpdateDevice } from '../../../use-cases/device/db-update-device-factory'
+import { makeDbLoadBrokerById } from '../../../use-cases/broker/db-load-broker-by-id-factory'
 import { makeUpdateDeviceValidator } from './update-device-validator-factory'
 
 export const makeUpdateDeviceController = (): UpdateDeviceController => {
@@ -9,6 +10,7 @@ export const makeUpdateDeviceController = (): UpdateDeviceController => {
     makeUpdateDeviceValidator(),
     makeDbLoadDeviceById(),
     makeDbCheckMemberPermission(),
+    makeDbLoadBrokerById(),
     makeDbUpdateDevice()
   )
 }
--- api/src/presentation/controllers/device/update-device-controller.ts
+++ api/src/presentation/controllers/device/update-device-controller.ts
@@ -2,25 +2,27 @@ import { Protocol } from '../../../domain/enums/protocol'
 import { Role } from '../../../domain/enums/role'
 import { LoadDeviceById } from '../../../domain/use-cases/device/load-device-by-id'
 import { CheckMemberPermission } from '../../../domain/use-cases/team/check-member-permission'
+import { LoadBrokerById } from '../../../domain/use-cases/broker/load-broker-by-id'
 import { ResourceNotFoundError } from '../../errors'
 import { badRequest, forbidden, notFound, ok, serverError } from '../../helpers/http-helper'
 import { Controller, HttpRequest, HttpResponse, Validator } from '../../protocols'
 import { UpdateDevice } from '../../../domain/use-cases/device/update-device'
 
 interface UpdateDeviceContract {
-    name?: string
-    protocol?: Protocol
-    mqttInfo?: {
-      topic: string
-      brokerId: string
-    }
+  name?: string
+  protocol?: Protocol
+  mqttInfo?: {
+    topic: string
+    brokerId: string
   }
+}
 
 export class UpdateDeviceController implements Controller {
   public constructor (
     private readonly validator: Validator,
     private readonly loadDeviceById: LoadDeviceById,
     private readonly checkMemberPermission: CheckMemberPermission,
+    private readonly loadBrokerById: LoadBrokerById,
     private readonly updateDevice: UpdateDevice
   ) {}
 
@@ -31,24 +33,30 @@ export class UpdateDeviceController implements Controller {
         return badRequest(error)
       }
 
-      const { userId } = httpRequest
+      const { userId, body: deviceData } = httpRequest
       const { deviceId } = httpRequest.params
-
-      const request = httpRequest.body
 
       const device = await this.loadDeviceById.load(deviceId)
       if (!device) {
         return notFound(new ResourceNotFoundError('device id'))
       }
 
-      const { accessGroupId } = device
-
-      const hasPermission = await this.checkMemberPermission.check(accessGroupId, userId, [Role.BASIC, Role.ADVANCED])
+      const hasPermission = await this.checkMemberPermission.check(device.accessGroupId, userId, [Role.BASIC, Role.ADVANCED])
       if (!hasPermission) {
         return forbidden()
       }
 
-      const updatedDevice = await this.updateDevice.update(deviceId, request)
+      const { mqttInfo } = deviceData
+
+      if (mqttInfo) {
+        const broker = await this.loadBrokerById.load(mqttInfo.brokerId, userId)
+
+        if (!broker) {
+          return notFound(new ResourceNotFoundError('broker id'))
+        }
+      }
+
+      const updatedDevice = await this.updateDevice.update(deviceId, deviceData)
 
       return ok(updatedDevice)
     } catch (error) {
