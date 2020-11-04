546fd3188eca2f2d493ee7cd98e6fc5d7a0e99ef
Flavio Brito <flavio.brito@sensedia.com>
2020-11-04 23:49:17 +0000

    feat: publish message in broker-connection-queue when broker is created

--- api/src/data/use-cases/broker/db-add-broker.ts
+++ api/src/data/use-cases/broker/db-add-broker.ts
@@ -2,14 +2,23 @@ import { AddBroker, AddBrokerModel } from '../../../domain/use-cases/broker/add-
 import { Broker } from '../../../domain/models/broker'
 import { AddBrokerRepository } from '../../protocols/db/broker/add-broker-repository'
 import { BrokerStatus } from '../../../domain/enums/broker-status'
+import { PublishBrokerConnectionQueue } from '../../protocols/message-queue/publish-broker-connection-queue'
+import { Action } from '../../../domain/enums/action'
 
 export class DbAddBroker implements AddBroker {
-  public constructor (private readonly addBrokerRepository: AddBrokerRepository) {}
+  public constructor (
+    private readonly addBrokerRepository: AddBrokerRepository,
+    private readonly publishBrokerConnectionQueue: PublishBrokerConnectionQueue
+  ) {}
 
   public async add (brokerData: AddBrokerModel): Promise<Broker> {
-    return await this.addBrokerRepository.add({
+    const broker = await this.addBrokerRepository.add({
       ...brokerData,
       status: BrokerStatus.PROCESSING
     })
+
+    await this.publishBrokerConnectionQueue.publishBrokerConnection(broker, Action.CONNECT)
+
+    return broker
   }
 }
