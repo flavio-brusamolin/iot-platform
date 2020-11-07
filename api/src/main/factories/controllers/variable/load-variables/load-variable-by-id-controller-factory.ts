39515e64b8c5bd0bdec73af46412e7f44174cfdd
Flavio Brito <flavio.brito@sensedia.com>
2020-11-07 12:48:54 +0000

    refactor: change file name

--- api/src/main/factories/controllers/variable/load-variables/load-variable-by-id-controller-factory.ts
+++ api/src/main/factories/controllers/variable/load-variables/load-variable-by-id-controller-factory.ts
@@ -1,6 +1,6 @@
 import { makeDbCheckMemberPermission } from '../../../use-cases/team/db-check-member-permission-factory'
 import { makeDbLoadVariableById } from '../../../use-cases/variable/db-load-variable-by-id-factory'
-import { LoadVariableByIdController } from '../../../../../presentation/controllers/variable/load-variable-by-id'
+import { LoadVariableByIdController } from '../../../../../presentation/controllers/variable/load-variable-by-id-controller'
 
 export const makeLoadVariableByIdController = (): LoadVariableByIdController => {
   return new LoadVariableByIdController(
