012b9e5a50e0eca9cfbe65940a66f1ad6340adad
Flavio Brito <flavio.brito@sensedia.com>
2020-11-21 19:07:18 +0000

    feat: add TokenInterceptor

---
+++ web/src/app/core/interceptors/token.interceptor.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core'
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http'
+
+import { Observable } from 'rxjs'
+
+import { AuthService } from '../services/auth.service'
+
+@Injectable()
+export class TokenInterceptor implements HttpInterceptor {
+  constructor (private authService: AuthService) { }
+
+  intercept (request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    const token = this.authService.getToken()
+
+    if (token) {
+      request = request.clone({
+        setHeaders: {
+          'x-access-token': token
+        }
+      })
+    }
+
+    return next.handle(request)
+  }
+}
