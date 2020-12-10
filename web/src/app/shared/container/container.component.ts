14e6983d8e5d9b8baab4d8d1d923e600bd121104
Flavio Brito <flavio.brito@sensedia.com>
2020-12-10 01:44:20 +0000

    feat: improve container title style

--- web/src/app/shared/container/container.component.ts
+++ web/src/app/shared/container/container.component.ts
@@ -1,11 +1,17 @@
 import { Component, Input, OnInit } from '@angular/core'
 
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons'
+
 @Component({
   selector: 'app-container',
   templateUrl: './container.component.html',
   styleUrls: ['./container.component.css']
 })
 export class ContainerComponent implements OnInit {
+  public readonly icons = {
+    arrow: faChevronRight
+  }
+
   @Input() public name!: string
   @Input() public complement?: string
 
