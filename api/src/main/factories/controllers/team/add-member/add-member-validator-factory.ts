2cbbbae5cb0d6c629070a935ddc802a5c75753d7
Flavio Brito <flavio.brito@sensedia.com>
2020-12-10 23:54:06 +0000

    fix: add 'required' validation in 'role' field

--- api/src/main/factories/controllers/team/add-member/add-member-validator-factory.ts
+++ api/src/main/factories/controllers/team/add-member/add-member-validator-factory.ts
@@ -5,7 +5,7 @@ import { JoiAdapter } from '../../../../adapters/joi/joi-adapter'
 export const makeAddMemberValidator = (): JoiAdapter => {
   const schema = joi.object({
     email: joi.string().required().email(),
-    role: joi.string().valid(...Object.values(Role))
+    role: joi.string().required().valid(...Object.values(Role))
   })
 
   return new JoiAdapter(schema)
